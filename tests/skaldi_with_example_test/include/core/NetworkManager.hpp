1d09da6d5db423c163eddfbec66fd95b9153fd15
fazanwolf <fazanwolf@gmail.com>
2023-03-01 10:05:05 +0400

    [FIX] Old refactor and [RM] TCP

---
+++ tests/skaldi_with_example_test/include/core/NetworkManager.hpp
@@ -0,0 +1,16 @@
+#ifndef SKALDI_NETWORK_MANAGER_HPP
+#define SKALDI_NETWORK_MANAGER_HPP
+
+namespace sk {
+
+    class NetworkManager {
+    public:
+        NetworkManager();
+        ~NetworkManager();
+    private:
+
+    };
+
+}
+
+#endif //SKALDI_NETWORK_MANAGER_HPP
